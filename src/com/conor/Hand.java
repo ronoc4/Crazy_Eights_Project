85571cab4c438b5d208bf04460fb6196ff9e03b7
Conor Budge <conorbudge555@gmail.com>
2016-03-21 18:45:42 -0500

    Added new methods to Hand class to remove card from ArrayList and add card from ArrayList of cards

--- src/com/conor/Hand.java
+++ src/com/conor/Hand.java
@@ -1,11 +1,10 @@
 package com.conor;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-
 
 /**
  * Created by conor on 3/6/16.
+ *
  */
 public class Hand {
     //Create set of cards
@@ -16,14 +15,14 @@ public class Hand {
         cards = new ArrayList<Card>();
     }
 
-    //methods for hand
-    public void add(Card card) {
-        cards.add(card);
-    }
-
     //Clear current hand
     public void clear() {
         cards.clear();
+    }
+
+    //methods for hand
+    public void add(Card card) {
+        cards.add(card);
     }
 
     //Show your hand
@@ -31,11 +30,21 @@ public class Hand {
         String str = "";
 
         for (Card c : cards) {
-            str += c.toString();
+            str += c.toString() + "\n"; //print each on new line
         }
 
         return str;
     }
 
+    public boolean giveCard(Card card, Hand otherHand) {
+        if(!cards.contains(card)) {
+            return false;
+        } else {
+            //Get index of card in deck
+            cards.remove(card);
+            otherHand.add(card);
+            return true;
+        }
+    }
 
 }
